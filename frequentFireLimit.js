6188719930c718cbf0e24da917a57771e8f9565e
K0V0 <kovo1533@gmail.com>
2017-11-24 22:02:18 +0100

    first commit

---
+++ frequentFireLimit.js
@@ -0,0 +1,16 @@
+(function($) {
+	$.fn.frequentFireLimit = function (event_type, delay, selector, callback) {
+
+		return this.each(function() {
+
+			var timed_object;
+			$(document).on(event_type, selector, function(e) {
+				var totok = this;
+				clearTimeout(timed_object);
+				timed_object = setTimeout(function() { $(totok).frequentFireLimit_handler(callback, e); }, delay);
+			});			
+		});
+	}
+
+	$.fn.frequentFireLimit_handler = function (h, evt) { h.call(this, evt); }
+}(jQuery));
