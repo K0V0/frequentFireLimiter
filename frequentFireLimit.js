37fa91fb04119fd744fa8909a8241a696882c464
K0V0 <kovo1533@gmail.com>
2017-11-25 02:08:26 +0100

    critical bug in code - using .frequentFireLimit(...) will ignore and bind it to  allways - repaired

--- frequentFireLimit.js
+++ frequentFireLimit.js
@@ -4,7 +4,7 @@
 		return this.each(function() {
 
 			var timed_object;
-			$(document).on(event_type, selector, function(e) {
+			$(this).on(event_type, selector, function(e) {
 				var totok = this;
 				clearTimeout(timed_object);
 				timed_object = setTimeout(function() { $(totok).frequentFireLimit_handler(callback, e); }, delay);
